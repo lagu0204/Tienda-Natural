af55c6d9d063a26cbf2aed7010fd52fa0c8e8f81
JuaN Diego <juadiegogarcialaguna@gmail.com>
2024-03-05 17:47:46 -0500

    2.0 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
 
-const routes: Routes = [];
+
+import { PrincipalComponent } from './modulos/principal/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '', component: PrincipalComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
